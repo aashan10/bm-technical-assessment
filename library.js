e92b141119e07adb36d9fbf5c461d6354216baae
Aashan Ghimire <ashanghimire10@gmail.com>
2023-05-22 13:46:23 +0545

    Added basic dom manipulation library

---
+++ library.js
@@ -0,0 +1,32 @@
+const BMLibrary = {
+    setClassName: function (element, className) {
+        element.classList.add(className);
+    },
+
+    removeClassName: function (element, className) {
+        element.classList.remove(className);
+    },
+
+    getDataset: function (element) {
+        return element.dataset;
+    },
+
+    appendChild: function (element, child) {
+        element.appendChild(child);
+    },
+
+    http: {
+        get: async function(url) {
+            return fetch(url, {
+                method: 'GET'
+            }).then(response => response.json())
+        },
+
+        post: async function(url, data) {
+            return fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data)
+            }).then(response => response.json())
+        }
+    }
+}
